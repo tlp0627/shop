dcbeb268d636157e0a474b54335baa4ccac8a939
tlp <335747378@qq.com>
2016-12-09 20:27:38 +0800

    master 2.0

--- src/com/shop/cart/vo/Cart.java
+++ src/com/shop/cart/vo/Cart.java
@@ -7,8 +7,23 @@ import java.util.Map;
 import com.shop.product.vo.Product;
 
 public class Cart {
+	private double total;//总计
+	//用于存储购物项，Map是为了方便取
 	private Map<Integer, CartItem> map = new LinkedHashMap<Integer, CartItem>();
 	
+	//添加购物项
+	public void addCart(CartItem cartItem){
+		Integer pid = cartItem.getProduct().getPid();
+		//如果map中已经包含了该CartItem项的情况
+		if(map.containsKey(pid)){
+			CartItem _carCartItem = map.get(pid);
+			cartItem.setCount(cartItem.getCount()+_carCartItem.getCount());
+		}
+		//如果map中未包含的情况
+		else {
+			map.put(cartItem.getProduct().getPid(), cartItem);
+		}
+	}
 	public Collection<CartItem>  getCartItema (){
 		return map.values();
 	}
@@ -18,5 +33,5 @@ public class Cart {
 	public void setTotal(double total) {
 		this.total = total;
 	}
-	private double total;
+
 }
