003a9b132ea288d9a8e98dfb633e7d75feaaffec
tlp <335747378@qq.com>
2016-12-12 17:59:58 +0800

    2.1  commit

---
+++ src/com/shop/order/vo/OrderItem.java
@@ -0,0 +1,64 @@
+package com.shop.order.vo;
+
+import java.util.Date;
+import java.util.jar.Attributes.Name;
+
+import org.hibernate.ejb.criteria.expression.function.AggregationFunction.COUNT;
+
+import com.shop.product.vo.Product;
+import com.shop.user.vo.User;
+
+
+/*
+ * 
+CREATE TABLE `orderitem` (
+  `itemid` int(11) NOT NULL AUTO_INCREMENT,
+  `count` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemid`),
+  KEY `FKE8B2AB6166C01961` (`oid`),
+  KEY `FKE8B2AB6171DB7AE4` (`pid`),
+  CONSTRAINT `FKE8B2AB6166C01961` FOREIGN KEY (`oid`) REFERENCES `orders` (`oid`),
+  CONSTRAINT `FKE8B2AB6171DB7AE4` FOREIGN KEY (`pid`) REFERENCES `product` (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+ */
+public class OrderItem {
+	private Integer itemid;
+	private Integer count;
+	private Double subtotal;
+	private Product product;
+	public Integer getItemid() {
+		return itemid;
+	}
+	public void setItemid(Integer itemid) {
+		this.itemid = itemid;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public Double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	private Order order;
+}
